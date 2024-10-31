88e92997136928623a506b358e851d3c72e6219c
Aleksei Gusev <aleksei.gusev@gmail.com>
2024-10-31 08:47:18 +0200

    Define outline structure

---
+++ languages/org/outline.scm
@@ -0,0 +1,3 @@
+(headline (stars) @name.stars (#match? @name.stars "^(\\*{3})*\\*$") (item)) @name @item
+(headline (stars) @name.stars (#match? @name.stars "^(\\*{3})*\\*\\*$") (item)) @name @item
+(headline (stars) @name.stars (#match? @name.stars "^(\\*{3})*\\*\\*\\*$") (item)) @name @item
